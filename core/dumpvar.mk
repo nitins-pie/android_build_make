3e8bac6acabd13923058e1251ecd0a954a07535c
RakeshBatra <raakesh.batra@rediffmail.com>
2018-05-18 01:56:33 +0000

    May Favourite Part ~ Flashing Script Added
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- core/dumpvar.mk
+++ core/dumpvar.mk
@@ -18,6 +18,68 @@ ANDROID_BUILD_PATHS := $(ABP)
 ANDROID_PREBUILTS := prebuilt/$(HOST_PREBUILT_TAG)
 ANDROID_GCC_PREBUILTS := prebuilts/gcc/$(HOST_PREBUILT_TAG)
 
+# The "dumpvar" stuff lets you say something like
+#
+#     CALLED_FROM_SETUP=true \
+#       make -f config/envsetup.make dumpvar-TARGET_OUT
+# or
+#     CALLED_FROM_SETUP=true \
+#       make -f config/envsetup.make dumpvar-abs-HOST_OUT_EXECUTABLES
+#
+# The plain (non-abs) version just dumps the value of the named variable.
+# The "abs" version will treat the variable as a path, and dumps an
+# absolute path to it.
+#
+dumpvar_goals := \
+	$(strip $(patsubst dumpvar-%,%,$(filter dumpvar-%,$(MAKECMDGOALS))))
+ifdef dumpvar_goals
+
+  ifneq ($(words $(dumpvar_goals)),1)
+    $(error Only one "dumpvar-" goal allowed. Saw "$(MAKECMDGOALS)")
+  endif
+
+  # If the goal is of the form "dumpvar-abs-VARNAME", then
+  # treat VARNAME as a path and return the absolute path to it.
+  absolute_dumpvar := $(strip $(filter abs-%,$(dumpvar_goals)))
+  ifdef absolute_dumpvar
+    dumpvar_goals := $(patsubst abs-%,%,$(dumpvar_goals))
+    DUMPVAR_VALUE := $(abspath $($(dumpvar_goals)))
+    dumpvar_target := dumpvar-abs-$(dumpvar_goals)
+  else
+    DUMPVAR_VALUE := $($(dumpvar_goals))
+    dumpvar_target := dumpvar-$(dumpvar_goals)
+  endif
+
+.PHONY: $(dumpvar_target)
+$(dumpvar_target):
+	@echo $(DUMPVAR_VALUE)
+
+endif # dumpvar_goals
+
+ifneq ($(dumpvar_goals),report_config)
+PRINT_BUILD_CONFIG:=
+endif
+
+ifneq ($(filter report_config,$(DUMP_MANY_VARS)),)
+# Construct the shell commands that print the config banner.
+report_config_sh := echo '============================================';
+report_config_sh += echo '   ▄████▄  ▒█████    ██████		      ';
+report_config_sh += echo '  ▒██▀ ▀█ ▒██▒  ██▒▒██    ▒ 		      ';
+report_config_sh += echo '  ▒▓█    ▄▒██░  ██▒░ ▓██▄                   ';
+report_config_sh += echo '  ▒▓▓▄ ▄██▒██   ██░  ▒   ██▒                ';
+report_config_sh += echo '  ▒ ▓███▀ ░ ████▓▒░▒██████▒▒                ';
+report_config_sh += echo '  ░ ░▒ ▒  ░ ▒░▒░▒░ ▒ ▒▓▒ ▒ ░                ';
+report_config_sh += echo '    ░  ▒    ░ ▒ ▒░ ░ ░▒  ░ ░                ';
+report_config_sh += echo '  ░       ░ ░ ░ ▒  ░  ░  ░                  ';
+report_config_sh += echo '  ░ ░         ░ ░        ░                  ';
+report_config_sh += echo '  ░                                         ';
+report_config_sh += echo '	ColtOS-$(COLT_VERSION) $(PLATFORM_VERSION) $(TARGET_PLATFORM_VERSION) ';
+report_config_sh := echo '============================================';
+report_config_sh += $(foreach v,$(print_build_config_vars),echo '$v=$($(v))';)
+report_config_sh += echo '============================================';
+endif
+
+>>>>>>> 7cb1e10... May Favourite Part ~ Flashing Script Added
 # Dump mulitple variables to "<var>=<value>" pairs, one per line.
 # The output may be executed as bash script.
 # Input variables:
